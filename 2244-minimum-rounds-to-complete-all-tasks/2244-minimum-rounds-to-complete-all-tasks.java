17ebd83f490727a8c60abdda96ef96ecfffac632
Rakesh Kumar <71805425+Rakeshkumar-7@users.noreply.github.com>
2023-01-04 14:30:12 +0530

    Time: 94 ms (33.56%), Space: 104.4 MB (37.22%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.java
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.java
@@ -1,24 +1,4 @@
 class Solution {
-    private int[] sumOf2And3(int n){
-        int a = (int)Math.ceil(n/3.0); // co. eff of 3
-        int b = 0; // co. eff of 2
-        int sum = 0;
-        while(a >= 0){
-            sum = 3*a + 2*b;
-            if(sum < n){
-                return new int[]{-1,-1};
-            }else if(sum > n){
-                a--;
-                b++;
-            }else{
-                break;
-            }
-        }
-        if(sum == n){
-            return new int[]{a,b};
-        }
-        return new int[]{-1,-1};
-    }
     public int minimumRounds(int[] tasks) {
         Map<Integer, Integer> count = new HashMap<>();
         for(int x: tasks){
@@ -30,15 +10,12 @@ class Solution {
 
         int i = 0;
         int rounds = 0;
-        // System.out.println(count);        
-        for(Map.Entry<Integer, Integer> entry: count.entrySet()){
-            int[] coeff = sumOf2And3(entry.getValue());
-            // System.out.println(entry.getValue());
-            // System.out.println(Arrays.toString(coeff));
-            if(coeff[0] == -1){
+        for(int c: count.values()){
+            if(c == 1){
                 return -1;
             }
-            rounds += coeff[0] + coeff[1];
+            rounds += c/3;
+            if(c % 3 != 0) rounds++;            
         }
         return rounds;
     }
