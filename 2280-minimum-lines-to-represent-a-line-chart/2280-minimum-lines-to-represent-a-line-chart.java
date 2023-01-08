ec10731e135c9b65845af8c8335ee144973198c9
Rakesh Kumar <71805425+Rakeshkumar-7@users.noreply.github.com>
2023-01-08 16:32:24 +0530

    Time: 40 ms (87.95%), Space: 89.5 MB (67.47%) - LeetHub

--- 2280-minimum-lines-to-represent-a-line-chart/2280-minimum-lines-to-represent-a-line-chart.java
+++ 2280-minimum-lines-to-represent-a-line-chart/2280-minimum-lines-to-represent-a-line-chart.java
@@ -8,9 +8,7 @@ class Solution {
         Arrays.sort(stockPrices, (a,b) -> Integer.compare(a[0], b[0]));
         int count = 1;
         for(int i=2; i<stockPrices.length; i++){
-            BigDecimal slope1 = (new BigDecimal(stockPrices[i-2][1] - stockPrices[i-1][1])).divide(new BigDecimal(stockPrices[i-2][0] - stockPrices[i-1][0]), MathContext.DECIMAL128);
-            BigDecimal slope2 = (new BigDecimal(stockPrices[i-1][1] - stockPrices[i][1])).divide(new BigDecimal(stockPrices[i-1][0] - stockPrices[i][0]), MathContext.DECIMAL128);
-            if(slope1.compareTo(slope2) != 0){
+            if((stockPrices[i-2][1] - stockPrices[i][1])*(stockPrices[i-1][0] - stockPrices[i][0]) != (stockPrices[i-1][1] - stockPrices[i][1])*(stockPrices[i-2][0] - stockPrices[i][0])){
                 count++;
             }
         }
