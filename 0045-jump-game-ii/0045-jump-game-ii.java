4822e45f187e2079930120056e3d80270f816e55
Rakesh Kumar <71805425+Rakeshkumar-7@users.noreply.github.com>
2023-02-08 19:25:12 +0530

    Time: 1 ms (99.45%), Space: 42.9 MB (48.55%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,22 +1,16 @@
 class Solution {
-    private int solve(int[] nums, int i, int[] dp){
-        if(i >= nums.length-1){
-            return 0;
-        }
-        if(dp[i] != 1000001){
-            return dp[i];
-        }
-        for(int j=1; j<=nums[i]; j++){
-            dp[i] = Math.min(dp[i], 1 + solve(nums, i + j, dp));
-        }
-        return dp[i];
-    }
     public int jump(int[] nums) {
-        int[] dp = new int[nums.length];
-        Arrays.fill(dp, 1000001);
-        dp[nums.length-1] = 0;
-        solve(nums, 0, dp);
-        System.out.println(Arrays.toString(dp));
-        return dp[0];
+        int jumps = 0;
+        int max = 0;
+        int end = 0;
+        for(int i=0; i<nums.length-1; i++){
+            max = Math.max(max, i + nums[i]);
+            // current sub-array is done
+            if(i == end){
+                end = max; // sub-array's window expands to max jump
+                jumps++; // we make a jump to max point/end
+            }
+        }
+        return jumps;
     }
 }
