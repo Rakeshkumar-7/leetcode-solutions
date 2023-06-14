54f14dabefccf82863dd1f858c70187dd290db07
Rakesh Kumar <71805425+Rakeshkumar-7@users.noreply.github.com>
2023-06-14 15:06:03 +0530

    Time: 1 ms (58.78%), Space: 43 MB (38.7%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -7,7 +7,6 @@ class Solution {
         for(int i=2; i<n; i++){
             dp[i] = cost[i] + Math.min(dp[i-1], dp[i-2]);
         }
-        System.out.println(Arrays.toString(dp));
         return Math.min(dp[n-1], dp[n-2]);
     }
 }
