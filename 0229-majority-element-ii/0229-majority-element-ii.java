fd123bafe13daa3e4517806c06faa088cfb5275b
Rakesh Kumar <71805425+Rakeshkumar-7@users.noreply.github.com>
2023-06-18 15:38:06 +0530

    Time: 2 ms (98.6%), Space: 47.3 MB (7.11%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,17 +1,37 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        Map<Integer, Integer> freq = new HashMap<>();
-        for(int x: nums){
-            freq.put(x, freq.getOrDefault(x, 0) + 1);
-        }
-        
-        List<Integer> majority = new LinkedList<>();
-        for(Map.Entry<Integer, Integer> entry: freq.entrySet()){
-            if(entry.getValue() > nums.length/3){
-                majority.add(entry.getKey());
+        int c1 = 0;
+        int el1 = -1;
+        int c2 = 0;
+        int el2 = -1;
+        for(int i=0; i<nums.length; i++){
+            if(c1 == 0 && nums[i] != el2){
+                el1 = nums[i];
+                c1 = 1;
+            }else if(c2 == 0 && nums[i] != el1){
+                el2 = nums[i];
+                c2 = 1;
+            }else if(nums[i] == el1) c1++;
+            else if(nums[i] == el2) c2++;
+            else{
+                c1--;
+                c2--;
             }
         }
+        
+        List<Integer> res = new LinkedList<>();
+        c1 = 0;
+        c2 = 0;
+        for(int x: nums){
+            if(x == el1){
+                c1++;
+            }else if(x == el2){
+                c2++;
+            }
+        }
+        if(c1 > nums.length/3) res.add(el1);
+        if(c2 > nums.length/3) res.add(el2);
 
-        return majority;
+        return res;
     }
 }
