9c0d6e887a95d112c5b9447f3820e59a11848f48
Rakesh Kumar <71805425+Rakeshkumar-7@users.noreply.github.com>
2023-06-19 13:08:05 +0530

    Time: 43 ms (84.92%), Space: 16.6 MB (85.26%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -2,6 +2,7 @@ class Solution:
     def generate(self, res: List[str], cur: str, n: int, _open: int):
         if(n == 0 and _open == 0):
             res.append(cur)
+            return
         if(n > 0):
             self.generate(res, cur + "(", n-1, _open+1)
         if(_open > 0):
