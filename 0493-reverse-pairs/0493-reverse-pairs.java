e93e2dec89aaa8738a02054ee1f130e219a15bce
Rakesh Kumar <71805425+Rakeshkumar-7@users.noreply.github.com>
2023-06-19 18:55:12 +0530

    Time: undefined (undefined), Space: undefined (undefined) - LeetHub

--- 0493-reverse-pairs/0493-reverse-pairs.java
+++ 0493-reverse-pairs/0493-reverse-pairs.java
@@ -1,16 +1,8 @@
 class Solution {
-    private int merge(int[] nums, int low, int mid, int high){
+    private void merge(int[] nums, int low, int mid, int high){
         int left = low;
         int right = mid+1;
-        
-        int count = 0;
-        for(int i=low; i<=mid; i++){
-            while(right <= high && nums[i] > 2*(long)nums[right]){
-                count += mid - i + 1;
-                right++;
-            }
-        }
-        right = mid + 1;
+       
         List<Integer> temp = new ArrayList<>();
         while(left <= mid && right <= high){
             if(nums[left] <= nums[right]){
@@ -30,18 +22,23 @@ class Solution {
         for(int i=low; i<=high; i++){
             nums[i] = temp.get(i-low);
         }
-
-        return count;
     }    
     private int mergeSort(int[] nums, int low, int high){
         int count = 0;
         if(high > low){
             int mid = (low + high)/2;
 
+            int right = mid+1;
             count += mergeSort(nums, low, mid);
             count += mergeSort(nums, mid+1, high);
-
-            count += merge(nums, low, mid, high);
+ 
+            for(int i=low; i<=mid; i++){
+                while(right <= high && nums[i] > 2*(long)nums[right]){
+                    count += mid - i + 1;
+                    right++;
+                }
+            }
+            merge(nums, low, mid, high);
         }
         return count;
     }
