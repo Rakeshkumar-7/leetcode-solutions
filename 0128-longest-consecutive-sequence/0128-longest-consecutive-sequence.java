cdec92e9438812efd62bd52f962090ee4b6d5e27
Rakesh Kumar <71805425+Rakeshkumar-7@users.noreply.github.com>
2023-06-20 20:14:16 +0530

    Time: 159 ms (43.31%), Space: 75.4 MB (5.18%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,30 +1,75 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
-        if(nums.length == 0){
-            return 0;
+        Set<Integer> set = new HashSet<>();
+        for(int x: nums){
+            set.add(x);
         }
-        PriorityQueue<Integer> heap = new PriorityQueue<>();
-        for(int num: nums){
-            heap.add(num);   
-        }
-        int max = 1;
-        int prev = heap.poll();
-        int length = 1;
-        
-        while(heap.size() > 0){
-            int cur = heap.poll();
-            
-            if(Math.abs(cur - prev) == 1){
-                length++;
-            }else if(cur == prev){
-                continue;
+        int max = 0;
+        UnionFind uf = new UnionFind(set.size());
+        for(int num: set){
+            if(set.contains(num - 1)){
+                uf.union(num-1, num);
+            }else if(set.contains(num + 1)){
+                uf.union(num+1, num);
             }else{
-                max = Math.max(max, length);
-                length = 1;
+                uf.getSize(num);
             }
-            prev = cur;
         }
-        max = Math.max(max, length);
+        
+        Map<Integer, Integer> size = uf.getSizes();
+        for(int x: size.values()){
+            max = Math.max(x, max);
+        }
+
         return max;
+    }
+}
+
+class UnionFind{
+    Map<Integer, Integer> size;
+    Map<Integer, Integer> parent;
+
+    UnionFind(int n){
+        size = new HashMap<>();
+        parent = new HashMap<>();
+    }
+
+    int getParent(int x){
+        if(parent.containsKey(x)){
+            if(parent.get(x) != x){
+                parent.put(x, this.getParent(parent.get(x)));
+            }
+            return parent.get(x);
+        }else{
+            parent.put(x, x);
+        }
+        return parent.get(x);
+    }
+
+    int getSize(int x){
+        size.put(x, size.getOrDefault(x, 1));
+        return size.get(x);
+    }
+
+    boolean union(int x, int y){
+        x = this.getParent(x);
+        y = this.getParent(y);
+        this.getSize(x);
+        this.getSize(y);
+        if(x == y){
+            return false;
+        }else if(size.get(x) >= size.get(y)){
+            size.put(x, size.get(x) + size.get(y));
+            parent.put(y, x);
+        }else{
+            size.put(y, size.get(y) + size.get(x));
+            parent.put(x, y);
+        }
+
+        return true;
+    }
+
+    Map<Integer, Integer> getSizes(){
+        return size;
     }
 }
